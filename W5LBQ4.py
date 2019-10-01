ea606bb13ce684cb6c05b06abe811accab9f437d
khushijain899 <56038894+khushijain899@users.noreply.github.com>
2019-10-01 21:32:34 +0530

    Add files via upload

---
+++ W5LBQ4.py
@@ -0,0 +1,5 @@
+base=int(input("enter base of parallelogram"))
+height=int(input("enter height of parallelogram"))
+area=base*height
+print("area of parallelogram is",area)
+input("press enter to continue")
